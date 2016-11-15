3306b74fe6b1db89bff9c37dc68592a9015b8b24
Aryanraut <aryanraut2015@gmail.com>
2016-11-15 10:21:20 +0545

    Add files via upload

---
+++ printyourname.c
@@ -0,0 +1,21 @@
+/*
+Student Name: Aryan Raut
+Roll No:
+Programm:WAP to print your name 10 Times.
+Lab Sheet :14
+Date:2016-11-15
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	  for(int i=0;i<10;i++){
+	
+	 printf("My Name Is Aryan\n");
+ }
+
+	
+return 0;
+}	
+	
